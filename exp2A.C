3e0551b98573dba8feb5216e91c647b4fb3230af
aditi dhama <Aditidhama26@gmail.com>
2026-09-12 16:49:18 +0530

    exp2a

--- exp2A.C
+++ exp2A.C
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 // Function to perform linear search
 int linearSearch(int arr[], int n, int key) {
     for (int i = 0; i < n; i++) {
@@ -8,14 +7,11 @@ int linearSearch(int arr[], int n, int key) {
     }
     return -1; // Return -1 if not found
 }
-
 int main() {
     int arr[] = {5, 12, 7, 25, 18};
     int n = sizeof(arr) / sizeof(arr[0]);
     int key = 25;
-
     int result = linearSearch(arr, n, key);
-
     if (result != -1)
         printf("Element %d found at index %d.\n", key, result);
     else
